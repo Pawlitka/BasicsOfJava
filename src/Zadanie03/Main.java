7cf3999c2459019df6675dd05cc7c6bf31afca28
pawelkapusta <pawelllosek@gmail.com>
2024-12-18 21:09:47 +0100

    base for exercise 3

---
+++ src/Zadanie03/Main.java
@@ -0,0 +1,7 @@
+package Zadanie03;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Zadanie03/Terminal.java
@@ -0,0 +1,8 @@
+package Zadanie03;
+
+public class Terminal {
+    public static int[][] calculateSquares(int sreenWidth, int sreenHeight, int side) {
+
+        return null;
+    }
+}
