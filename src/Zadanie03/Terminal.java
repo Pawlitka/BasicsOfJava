f544113a8982528bba6fac9c94627fba0b14698c
pawelkapusta <pawelllosek@gmail.com>
2024-12-18 21:54:36 +0100

    completed exercise 3

--- src/Zadanie03/Terminal.java
+++ src/Zadanie03/Terminal.java
@@ -1,8 +1,20 @@
 package Zadanie03;
 
 public class Terminal {
-    public static int[][] calculateSquares(int sreenWidth, int sreenHeight, int side) {
+    public static int[][] calculateSquares(int screenWidth, int screenHeight, int side) {
+        int x = screenWidth / side;
+        int y = screenHeight / side;
+        int numberOfSquares = x * y;
 
-        return null;
+        int[][] tableOfSquares = new int[numberOfSquares][2];
+        int index = 0;
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                tableOfSquares[index][0] = i;
+                tableOfSquares[index][1] = j;
+                index++;
+            }
+        }
+        return tableOfSquares;
     }
 }
