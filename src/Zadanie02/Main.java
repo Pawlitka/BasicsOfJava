062dbc00e991e7b7f31e90b6eec2d575e22702fc
pawelkapusta <pawelllosek@gmail.com>
2024-12-18 21:07:21 +0100

    complete exercise 2

--- src/Zadanie02/Main.java
+++ src/Zadanie02/Main.java
@@ -1,4 +1,20 @@
 package Zadanie02;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] arr1 = new int[rand.nextInt(10) + 1];
+        int[] arr2 = new int[rand.nextInt(10) + 1];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = rand.nextInt(10) + 1;
+            arr2[i] = rand.nextInt(10) + 1;
+        }
+
+        Terminal terminal = new Terminal();
+        System.out.println(Arrays.toString(terminal.method(arr1, arr2, -2)));
+    }
 }
