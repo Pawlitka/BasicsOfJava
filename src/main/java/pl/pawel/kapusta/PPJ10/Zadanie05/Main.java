5defb9432a74c18277993e077e55db6aa1352a41
pawelkapusta <pawelllosek@gmail.com>
2025-01-03 18:30:16 +0100

    Add final exercise 5

--- src/main/java/pl/pawel/kapusta/PPJ10/Zadanie05/Main.java
+++ src/main/java/pl/pawel/kapusta/PPJ10/Zadanie05/Main.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Terminal.fiboR(5));
+        System.out.println(Terminal.fiboR(45));
 
-        System.out.println(Terminal.factR(5));
+        System.out.println(Terminal.factR(12));
 
+        System.out.println(Terminal.gdcR(12125, 40643));
+
+        int[] a = {3,8,2,9,7,4};
+        System.out.println("Max : " + Terminal.maxElem(a,0));
+        System.out.println("Num even : " + Terminal.numEven(a,0));
     }
 }
--- src/main/java/pl/pawel/kapusta/PPJ10/Zadanie05/Terminal.java
+++ src/main/java/pl/pawel/kapusta/PPJ10/Zadanie05/Terminal.java
@@ -1,6 +1,9 @@
 package pl.pawel.kapusta.PPJ10.Zadanie05;
 
 public class Terminal {
+    public static int maxNumberInArray = 0;
+    public static int evenNumbersCounter = 0;
+
     public static int fiboR(int n) {
         if(n == 0) {
             return 0;
@@ -23,6 +26,39 @@ public class Terminal {
         return n * factR(n - 1);
 
 
+    }
+
+    public static int gdcR(int a, int b) {
+        if(b == 0){
+            return a;
+        } else {
+            return gdcR(b, a % b);
+        }
+    }
+
+    public static int maxElem(int[] arr, int from) {
+        if(from == arr.length) {
+            return maxNumberInArray;
+        }
+
+        if(maxNumberInArray < arr[from]) {
+            maxNumberInArray = arr[from];
+        }
+
+        return maxElem(arr, from + 1);
+    }
+
+
+
+    public static int numEven(int[] arr, int from) {
+        if(from == arr.length) {
+            return evenNumbersCounter;
+        }
+        if(arr[from] % 2 == 0) {
+            evenNumbersCounter++;
+        }
+
+        return numEven(arr, from + 1);
     }
 
 }
